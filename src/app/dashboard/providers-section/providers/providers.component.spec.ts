05e5967c89cbeca1ea7164fa90eaaf424fbb2009
Ari Vincenti <arivincenti@gmail.com>
2020-12-01 19:18:17 -0300

    update

--- src/app/dashboard/providers-section/providers/providers.component.spec.ts
+++ src/app/dashboard/providers-section/providers/providers.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProvidersComponent } from './providers.component';
 
@@ -6,7 +6,7 @@ describe('ProvidersComponent', () => {
   let component: ProvidersComponent;
   let fixture: ComponentFixture<ProvidersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProvidersComponent ]
     })
